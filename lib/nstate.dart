c01717b9cccaa20bd8807618ec760b66b3a124c9
NOTKATEAMmainAndroidDeveloper <ntk.team.developer@gmail.com>
2025-08-20 14:01:10 +0300

    fix rebuild controller after rebuild widget

--- lib/nstate.dart
+++ lib/nstate.dart
@@ -1,68 +1,61 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:nstater/ncontroller.dart';
+
+import 'ncontroller.dart';
 
 class NState<T> extends StatelessWidget {
   final T Function() create;
   final Widget Function(T controller) builder;
-  final NController _controller;
 
-  NController get controller => _controller;
-
-  NState({super.key, required this.create, required this.builder})
-      : _controller = create() as NController;
+  const NState({super.key, required this.create, required this.builder});
 
   @override
-  Widget build(BuildContext context) => builder(_controller as T);
+  Widget build(BuildContext context) => const SizedBox.shrink();
 
   @override
   StatelessElement createElement() => _NStateElement<T>(this);
 }
 
 class _NStateElement<T> extends StatelessElement {
-  bool _isFirstBuild = true;
   _NStateElement(NState super.widget);
-  NState get _widget => widget as NState;
+
+  NState<T> get _widget => widget as NState<T>;
+
+  late final NController _controller = _widget.create() as NController;
 
   @override
   void mount(Element? parent, Object? newSlot) {
-    _widget.controller.beforeMount();
+    _controller.beforeMount();
     super.mount(parent, newSlot);
-    _widget.controller.onInit();
-    _widget.controller.addListener(_listener);
+    _controller.onInit();
+    _controller.addListener(_listener);
 
     scheduleMicrotask(() {
-      if (mounted) _widget.controller.onReady();
+      if (mounted) _controller.onReady();
     });
   }
 
   @override
   Widget build() {
-    final builtWidget = super.build();
-    if (_isFirstBuild) _isFirstBuild = false;
-    return builtWidget;
+    return _widget.builder(_controller as T);
   }
 
   @override
   void update(StatelessWidget newWidget) {
-    final oldWidget = _widget;
     super.update(newWidget);
-    if (oldWidget.controller != _widget.controller) {
-      oldWidget.controller.removeListener(_listener);
-      _widget.controller.addListener(_listener);
-    }
   }
 
   @override
   void unmount() {
-    _widget.controller.removeListener(_listener);
-    _widget.controller.unmount();
+    _controller.removeListener(_listener);
+    _controller.unmount();
     super.unmount();
   }
 
   void _listener() {
-    if (mounted) markNeedsBuild();
+    if (mounted) {
+      markNeedsBuild();
+    }
   }
 }
-
